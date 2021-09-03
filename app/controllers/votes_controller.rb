d3bcc442781afaf1847735b5d94bccfdf698000d
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2021-09-03 12:47:22 +0300

    Restrict multiple votes using a cookie

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,42 @@
+require 'json'
+
 class VotesController < ApplicationController
-	def update
+  def update
+
+    if vote_params.nil?
+      return redirect_to :root
+    end
+
     begin
       @answer = Answer.find(vote_params["vote"])
     rescue ActiveRecord::RecordNotFound
       redirect_to :root
+    end
+
+    # Restrict vote using a cookie
+    if cookies[:num_votes].nil?
+      cookies[:num_votes] = { :num_votes => [ { :survey_id => @answer.survey_id, :vote => true } ] }.to_json
+    else
+      num_votes_cookie = JSON.parse(cookies[:num_votes])
+      is_survey_in_cookie = false
+
+      num_votes_cookie["num_votes"].each do |value|
+        if value["survey_id"] == @answer.survey_id
+          is_survey_in_cookie = true
+          if value["vote"]
+            flash[:danger] = "You can only vote once"
+            return redirect_to survey_path(@answer.survey_id)
+          else
+            num_votes_cookie["num_votes"].append( { :survey_id => @answer.survey_id, :vote => true } )
+            cookies[:num_votes] = num_votes_cookie.to_json
+          end
+        end
+      end
+
+      unless is_survey_in_cookie
+        num_votes_cookie["num_votes"].append( { :survey_id => @answer.survey_id, :vote => true } )
+        cookies[:num_votes] = num_votes_cookie.to_json
+      end
     end
 
     if @answer.update({ :votes => @answer.votes + 1 })
@@ -16,6 +49,10 @@ class VotesController < ApplicationController
 
   private
   def vote_params
-		params.require(:post)
-	end
+    begin
+      params.require(:post)
+    rescue ActionController::ParameterMissing
+      return nil
+    end
+  end
 end
