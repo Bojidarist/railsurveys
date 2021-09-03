49776df79d1edd81a301619ed932091ea4265158
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2021-09-03 15:54:56 +0300

    Refactor VotesController

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,46 +14,10 @@ class VotesController < ApplicationController
       redirect_to :root
     end
 
-    # Restrict vote using a cookie
-    if cookies[:num_votes].nil?
-      cookies[:num_votes] = { :num_votes => [ { :survey_id => @answer.survey_id, :vote => true } ] }.to_json
-    else
-      num_votes_cookie = JSON.parse(cookies[:num_votes])
-      is_survey_in_cookie = false
-
-      num_votes_cookie["num_votes"].each do |value|
-        if value["survey_id"] == @answer.survey_id
-          is_survey_in_cookie = true
-          if value["vote"]
-            flash[:danger] = "You can only vote once"
-            return redirect_to survey_path(@answer.survey_id)
-          else
-            num_votes_cookie["num_votes"].append( { :survey_id => @answer.survey_id, :vote => true } )
-            cookies[:num_votes] = num_votes_cookie.to_json
-          end
-        end
-      end
-
-      unless is_survey_in_cookie
-        num_votes_cookie["num_votes"].append( { :survey_id => @answer.survey_id, :vote => true } )
-        cookies[:num_votes] = num_votes_cookie.to_json
-      end
+    if votes_restricted?(@answer, @survey)
+      flash[:danger] = "You cannot vote anymore"
+      return redirect_to @survey
     end
-
-
-    # Restrict vote using ip
-    geo_vote = @survey.geo_votes.find_by(ip_address: request.remote_ip)
-    if geo_vote.nil?
-      @survey.geo_votes.create(ip_address: request.remote_ip, votes: 1)
-    else
-      if geo_vote.votes >= 5
-        flash[:danger] = "You cannot vote anymore"
-        return redirect_to @survey
-      else
-        geo_vote.update({ :votes => geo_vote.votes + 1 })
-      end
-    end
-
     
     if @answer.update({ :votes => @answer.votes + 1 })
       redirect_to result_survey_path(@survey.id)
@@ -69,5 +33,9 @@ class VotesController < ApplicationController
     rescue ActionController::ParameterMissing
       return nil
     end
+  end
+
+  def votes_restricted?(answer, survey)
+    return !helpers.can_vote_cookie?(answer) || !helpers.can_vote_ip?(survey)
   end
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,44 @@
 module VotesHelper
+  def can_vote_cookie?(answer)
+    if cookies[:num_votes].nil?
+      cookies[:num_votes] = { :num_votes => [ { :survey_id => answer.survey_id, :vote => true } ] }.to_json
+    else
+      num_votes_cookie = JSON.parse(cookies[:num_votes])
+      is_survey_in_cookie = false
+
+      num_votes_cookie["num_votes"].each do |value|
+        if value["survey_id"] == answer.survey_id
+          is_survey_in_cookie = true
+          if value["vote"]
+            return false
+          else
+            num_votes_cookie["num_votes"].append( { :survey_id => answer.survey_id, :vote => true } )
+            cookies[:num_votes] = num_votes_cookie.to_json
+          end
+        end
+      end
+
+      unless is_survey_in_cookie
+        num_votes_cookie["num_votes"].append( { :survey_id => answer.survey_id, :vote => true } )
+        cookies[:num_votes] = num_votes_cookie.to_json
+      end
+    end
+
+    return true
+  end
+
+  def can_vote_ip?(survey)
+    geo_vote = survey.geo_votes.find_by(ip_address: request.remote_ip)
+    if geo_vote.nil?
+      survey.geo_votes.create(ip_address: request.remote_ip, votes: 1)
+    else
+      if geo_vote.votes >= 5
+        return false
+      else
+        geo_vote.update({ :votes => geo_vote.votes + 1 })
+      end
+    end
+
+    return true
+  end
 end
