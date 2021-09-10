42e834bda6dbfc5b81002653dbacea175c2fb371
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2021-09-10 10:16:47 +0300

    Add status to surveys
    Surveys can have active, pending, and rejected status

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,14 @@
 class Survey < ApplicationRecord
-	has_many :answers, dependent: :delete_all, inverse_of: :survey
-	has_many :geo_votes, dependent: :delete_all
-	belongs_to :user
-	validates :question, presence: true, length: { minimum: 10 }
-	accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+    enum status: [:active, :pending, :rejected]
+    after_initialize :set_default_status, :if => :new_record?
+
+    has_many :answers, dependent: :delete_all, inverse_of: :survey
+    has_many :geo_votes, dependent: :delete_all
+    belongs_to :user
+    validates :question, presence: true, length: { minimum: 10 }
+    accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+    def set_default_status
+        self.status ||= :pending
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_132239) do
+ActiveRecord::Schema.define(version: 2021_09_10_070104) do
 
   create_table "answers", force: :cascade do |t|
     t.text "description"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_09_09_132239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status"
     t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
