f70bac31e9cad424cce0cb202513f0b1bf6d0d69
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2021-09-18 14:21:17 +0300

    Add admin user seed to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create admin user with random password (change it with 'forgot password')
+randomPassword = Devise.friendly_token.first(8)
+user = User.new(
+  email: "admin@railsurveys.com", username: "adminuser",
+  password: randomPassword, password_confirmation: randomPassword,
+  role: "admin"
+).save!
